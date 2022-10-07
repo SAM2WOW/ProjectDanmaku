f795b24804bbc9a34d97e293949a6fe19b342c90
Menace791 <menacemenace791@gmail.com>
2022-10-07 14:05:18 -0700

    Created a portal that switches verses; Can also use 0, 1, and 2 keys for debugging

--- objects/weapons/PlayerBullet.gd
+++ objects/weapons/PlayerBullet.gd
@@ -11,3 +11,18 @@ func _on_PlayerBullet_body_entered(body):
 		body.damage(10)
 		
 		queue_free()
+
+func _on_Verse_Jump(verse):
+	match verse:
+		0:
+			get_node("Style0").show()
+			get_node("Style1").hide()
+			get_node("Style2").hide()
+		1:
+			get_node("Style0").hide()
+			get_node("Style1").show()
+			get_node("Style2").hide()
+		2:
+			get_node("Style0").hide()
+			get_node("Style1").hide()
+			get_node("Style2").show()
