19597d2fab4d4d962d4f33abd07c0e0d5191cae0
Sam Feng <fengyuan20000303@gmail.com>
2022-10-09 01:09:54 -0700

    Portal Bug FIxed

--- objects/system/portal.gd
+++ objects/system/portal.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
 
-var player_style = 0
+var previous_style = {}
 export var style = 0
 
 
 func _on_Area2D_body_entered(body):
 	if body.has_method('_on_Verse_Jump'):
-		player_style = body.style
+		previous_style[body.name] = body.style
 		body._on_Verse_Jump(style)
 
 
 func _on_Area2D_body_exited(body):
 	if body.has_method('_on_Verse_Jump'):
-		body._on_Verse_Jump(player_style)
+		body._on_Verse_Jump(previous_style[body.name])
+		previous_style.erase(body.name)
