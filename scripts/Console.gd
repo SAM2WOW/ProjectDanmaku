8ebefd86664e69a47bbe5745ffd1d980085dcf42
Sam Feng <fengyuan20000303@gmail.com>
2022-10-09 19:35:23 -0700

    Added Boss Health

--- scripts/Console.gd
+++ scripts/Console.gd
@@ -1,8 +1,17 @@
 extends Node
 
 
-var boss_health = 100
+var boss_health = 1000
 
 
 func _ready():
 	Global.console = self
+	
+	$"../CanvasLayer/Control/HealthBar".set_max(boss_health)
+	$"../CanvasLayer/Control/HealthBar".set_value(boss_health)
+
+
+func damage_boss(amount):
+	boss_health -= amount
+	
+	$"../CanvasLayer/Control/HealthBar".set_value(boss_health)
