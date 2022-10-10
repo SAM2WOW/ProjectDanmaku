fc3a0f7b9c8b191b96a0d17dbe8763a1f3170267
Sam Feng <fengyuan20000303@gmail.com>
2022-10-09 19:28:05 -0700

    Update portal.gd

--- objects/system/portal.gd
+++ objects/system/portal.gd
@@ -7,6 +7,10 @@ export var style = 0
 
 func _ready():
 	$Sprite.set_material(load("res://arts/shaders/Style%d.tres" % style))
+	
+	set_scale(Vector2(0, 0))
+	var tween = create_tween().set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "scale", Vector2(1, 1), 1)
 
 
 func _on_Area2D_body_entered(body):
