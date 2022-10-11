1fc1efdd859f7957123b0d464fba5c76784b8066
theweisa <weiandrew2012@gmail.com>
2022-10-10 19:00:39 -0700

    attempt change

--- objects/weapons/PlayerBullet.gd
+++ objects/weapons/PlayerBullet.gd
@@ -4,6 +4,9 @@ var style = Global.initial_style
 var damage = 10;
 var dir = Vector2();
 var bullet = load("res://objects/weapons/PlayerBullet.tscn");
+var bullet_properties = Global.player_bullet_properties[style];
+var explosion = preload("res://objects/weapons/PlayerExplosion.tscn");
+var curr_vel = 0.0;
 
 func _ready():
 	pass;
@@ -13,22 +16,27 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 
 func _on_PlayerBullet_body_entered(body):
-	print("Collide %s" % body.name)
+	print("Bullet Collide %s" % body.name)
 	if "Boss" in body.name:
-		body.damage(damage)
+		# spawns an explosion at collision area if pixel bullet
+		if (style==1):
+			var e = explosion.instance();
+			e.get_node("AnimatedSprite").play();
+			get_tree().root.add_child(e);
+			e.set_global_position(get_global_position());
+			e.damage = damage;
+		else:
+			body.damage(damage)
 		
 		queue_free()
 
-
-func _on_Verse_Jump(verse):
-	style = verse
+func show_verse_style(verse):
 	# show all the styles
-	get_node("Style%d" % style).show()
+	get_node("Style%d" % verse).show()
 	# hide the styles that aren't said style
 	for i in range(Global.total_style):
-		if i != style:
+		if i != verse:
 			get_node("Style%d" % i).hide()
-			#print("Style%d" % style)
 
 func _on_ExplodeTimer_timeout():
 	# queue_free();
@@ -36,7 +44,7 @@ func _on_ExplodeTimer_timeout():
 	print("explode!");
 
 func _physics_process(delta):
-	pass;
+	curr_vel = sqrt(pow(linear_velocity.x,2)+pow(linear_velocity.y,2));
 
 # bursts after 1/2 way to dest perhaps
 func pixel_verse_properties():
